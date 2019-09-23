2db8eb13527ce5d103dd96c843a217c335423e37
Ciro Laptop <c.losapio@ilpas.it>
2019-09-23 22:16:04 +0200

    update demo

--- demo/src/layouts/MyLayout.vue
+++ demo/src/layouts/MyLayout.vue
@@ -13,31 +13,41 @@
     </q-header>
 
     <q-drawer v-model="leftDrawerOpen" bordered content-class="bg-grey-2">
+      <q-item-label header>Menu</q-item-label>
       <q-list>
-        <q-item-label header>Menu</q-item-label>
-        <q-item clickable tag="a" target="_blank" href="https://github.com/heartbeatLV/app-extension-q-filter-configurator">
-          <q-item-section avatar>
-            <q-icon name="mdi-github-circle" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>QFilterConfigurator</q-item-label>
-            <q-item-label caption>@app-extension-q-filter-configurator</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://github.com/heartbeatLV/app-extension-q-date-filter">
-          <q-item-section avatar>
-            <q-icon name="mdi-github-circle" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>QDateFilter</q-item-label>
-            <q-item-label caption>@app-extension-q-date-filter</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-separator spaced />
-        <q-expansion-item default-opened icon="link" label="Essential Links" caption="@quasar">
-          <q-item clickable tag="a" target="_blank" href="https://github.com/quasarframework/">
+        <q-expansion-item group="links" expand-separator default-opened icon="mdi-github-circle" label="Other App Extensions" caption="@heartbeatLV" dark header-class="bg-teal">
+          <q-item clickable tag="a" target="_blank" href="https://github.com/heartbeatLV/app-extension-q-filter-configurator" class="text-teal">
             <q-item-section avatar>
-              <q-icon name="code" />
+              <q-icon name="tune" color="teal" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>QFilterConfigurator</q-item-label>
+              <q-item-label caption>q-filter-configurator</q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-item clickable tag="a" target="_blank" href="https://github.com/heartbeatLV/app-extension-q-date-filter" class="text-teal">
+            <q-item-section avatar>
+              <q-icon name="mdi-calendar-blank" color="teal" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>QDateFilter</q-item-label>
+              <q-item-label caption>q-date-filter</q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-expansion-item>
+        <q-expansion-item group="links" expand-separator icon="link" label="Quasar Framework" caption="@quasar" dark header-class="bg-primary">
+          <q-item clickable tag="a" target="_blank" href="https://quasar.dev">
+            <q-item-section avatar>
+              <q-icon name="school" color="blue" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Docs</q-item-label>
+              <q-item-label caption>quasar.dev</q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-item clickable tag="a" target="_blank" href="https://github.quasar.dev">
+            <q-item-section avatar>
+              <q-icon name="code" color="blue" />
             </q-item-section>
             <q-item-section>
               <q-item-label>Github</q-item-label>
@@ -46,7 +56,7 @@
           </q-item>
           <q-item clickable tag="a" target="_blank" href="https://chat.quasar.dev">
             <q-item-section avatar>
-              <q-icon name="chat" />
+              <q-icon name="chat" color="blue" />
             </q-item-section>
             <q-item-section>
               <q-item-label>Discord Chat Channel</q-item-label>
@@ -55,7 +65,7 @@
           </q-item>
           <q-item clickable tag="a" target="_blank" href="https://forum.quasar.dev">
             <q-item-section avatar>
-              <q-icon name="record_voice_over" />
+              <q-icon name="record_voice_over" color="blue" />
             </q-item-section>
             <q-item-section>
               <q-item-label>Forum</q-item-label>
@@ -64,7 +74,7 @@
           </q-item>
           <q-item clickable tag="a" target="_blank" href="https://twitter.com/quasarframework">
             <q-item-section avatar>
-              <q-icon name="rss_feed" />
+              <q-icon name="rss_feed" color="blue" />
             </q-item-section>
             <q-item-section>
               <q-item-label>Twitter</q-item-label>
@@ -73,7 +83,7 @@
           </q-item>
           <q-item clickable tag="a" target="_blank" href="https://facebook.quasar.dev">
             <q-item-section avatar>
-              <q-icon name="public" />
+              <q-icon name="public" color="blue" />
             </q-item-section>
             <q-item-section>
               <q-item-label>Facebook</q-item-label>
@@ -91,11 +101,11 @@
 </template>
 
 <script>
-import { openURL, QExpansionItem, QSeparator } from 'quasar'
+import { openURL, QExpansionItem } from 'quasar'
 
 export default {
   name: 'MyLayout',
-  components: { QExpansionItem, QSeparator },
+  components: { QExpansionItem },
   data () {
     return {
       leftDrawerOpen: this.$q.platform.is.desktop
@@ -107,5 +117,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="stylus" scoped>
+.q-expansion-item__content a
+  padding-left 25px
 </style>
